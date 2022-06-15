f52430b35a8a95e027a2485eb6c32da008fa3929
Rajat Kumar Nayak <rajatnayak1582002@gmail.com>
2022-06-15 16:00:07 +0530

    commit

--- todo_drf/settings.py
+++ todo_drf/settings.py
@@ -11,7 +11,7 @@ https://docs.djangoproject.com/en/4.0/ref/settings/
 """
 
 import django_heroku
-
+import os
 
 from pathlib import Path
 
@@ -127,5 +127,7 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 
 django_heroku.settings(locals())
